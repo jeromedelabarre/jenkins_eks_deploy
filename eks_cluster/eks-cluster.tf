3093af2edec8adc0e4576b1a157ceffbcd66efe3
Cley Pal <cley@paloaltonetworks.com>
2023-03-13 06:02:09 -0700

    Add 3 nodes to eks cluster

--- eks_cluster/eks-cluster.tf
+++ eks_cluster/eks-cluster.tf
@@ -22,7 +22,7 @@ module "eks" {
 
       min_size     = 1
       max_size     = 5
-      desired_size = 1
+      desired_size = 3
     }
 
     two = {
@@ -32,7 +32,7 @@ module "eks" {
 
       min_size     = 1
       max_size     = 3
-      desired_size = 1
+      desired_size = 2
     }
   }
 }
