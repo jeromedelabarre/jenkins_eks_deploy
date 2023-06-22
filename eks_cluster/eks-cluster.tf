e951fea668962f718db477f4f5186e5ea0e0bb5a
chrisley75 <cley@paloaltonetworks.com>
2023-06-22 14:50:26 +0200

    delete cluster

--- eks_cluster/eks-cluster.tf
+++ eks_cluster/eks-cluster.tf
@@ -18,8 +18,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t2.medium"]
-
+      instance_types = ["t3.medium"]
       min_size     = 1
       max_size     = 5
       desired_size = 3
@@ -28,7 +27,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
-      instance_types = ["t2.medium"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 3
