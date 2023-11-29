13515b3da5236f250dda70d784cd00e3fb0449fe
jeromedelabarre <89081166+jeromedelabarre@users.noreply.github.com>
2023-11-29 10:59:21 +0100

    Update main.tf

--- eks_cluster/main.tf
+++ eks_cluster/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "cley-eks-${random_string.suffix.result}"
+  cluster_name = "jej-eks-${random_string.suffix.result}"
 }
 
 
